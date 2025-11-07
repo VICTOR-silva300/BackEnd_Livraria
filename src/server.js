513292b6e7982f92ff0b04965fc88d1832384f8d
VICTOR-silva300 <victor.h.silva119@aluno.senai.br>
2025-11-07 15:51:30 -0300

    server.js

--- src/server.js
+++ src/server.js
@@ -5,16 +5,21 @@ import livrosRoutes from './routes/livros.routes.js';
 import avaliacoesRoutes from './routes/avaliacoes.routes.js';
 import usuariosRoutes from './routes/usuarios.routes.js';
 
+
+
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 app.use(express.json());
 
+
 app.use('/livros', livrosRoutes);
 app.use('/avaliacoes', avaliacoesRoutes);
 app.use('/usuarios', usuariosRoutes);
 
+
+
 app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`✅ Servidor rodando em http://localhost:${PORT}`);
 });
